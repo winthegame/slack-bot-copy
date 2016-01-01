279b75c488095d8de952fbec9325f57b4d9bb8c1
Rafael Cosman <rafaelcosman@gmail.com>
2016-01-01 21:55:49 +0000

    adds comment

--- slack-bot-copy.js
+++ slack-bot-copy.js
@@ -26,7 +26,7 @@ controller.configureSlackApp({
   clientId: process.env.clientId,
   clientSecret: process.env.clientSecret,
   redirect_uri: 'http://localhost:3002',
-  scopes: ['team:read','users:read','channels:read','im:read','im:write','groups:read','emoji:read']
+  scopes: ['team:read','users:read','channels:read','im:read','im:write','groups:read','emoji:read'] //TODO check that these are the right scopes.
 });
 
 controller.setupWebserver(process.env.PORT, function(err,webserver) {

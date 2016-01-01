cc95b14664780a36907ff900a8efb6df57c49609
Rafael Cosman <rafaelcosman@gmail.com>
2016-01-01 18:33:16 +0000

    storage

--- slack-bot-copy.js
+++ slack-bot-copy.js
@@ -23,7 +23,7 @@ if (!process.env.token) {
 
 var controller = Botkit.slackbot({
   debug: false,
-  json_file_store: './db_copybot/'
+  json_file_store: process.env.json_file_store
 });
 
 controller.configureSlackApp({
